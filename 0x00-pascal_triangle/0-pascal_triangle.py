398ef2196c0d19599be63d1179ffc69f9ec3b34a
Kim_Farida <farimomoh@gmail.com>
2023-01-26 22:40:24 +0000

    Task done

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,17 +3,20 @@
 0-pascal_triangle
 """
 def pascal_triangle(n):
+
     triangle = []
 
     if n <= 0:
         return triangle
 
-    triangle =[[1]]
-    for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)
-        triangle.append(row)
+    for i in range(n):
+        temp_list = []
+
+        for j in range(i+1):
+            if j == 0 or j == 1:
+                temp_list.append(1)
+            else:
+                temp_list.append(triangle[i-1][j-1] + triangle[i-1][j])
+        triangle.append(temp_list)
 
     return triangle
